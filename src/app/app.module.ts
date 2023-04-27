c9563f4c13530ea71bf8fa2b1af1d4943740dde3
Mark Giannelli <giannellimj@gmail.com>
2023-04-27 16:42:48 -0400

    write logic for custom btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ButtonComponent
+    ButtonComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
